64f2fda80a48ec81d5bd059d50d1ad867cdb45c0
lefterisspetsakis <left.plan@gmail.com>
2017-02-07 19:40:46 -0500

    Rotation limitation removed (to be added later)

--- JumpingBlock/Assets/PlayerController.cs
+++ JumpingBlock/Assets/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
 	void Update ()
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 300.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
