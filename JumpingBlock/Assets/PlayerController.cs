1500c2db24ce8c8b3fd349a99c4294770a57cfa8
lefterisspetsakis <left.plan@gmail.com>
2017-02-07 19:25:16 -0500

    Initial commit

---
+++ JumpingBlock/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Quaternion addRot;
+	// Use this for initialization
+	void Start ()
+    {
+        this.rb = GetComponent<Rigidbody>();
+        this.addRot = Quaternion.identity;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 300.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+        }
+    }
+}
