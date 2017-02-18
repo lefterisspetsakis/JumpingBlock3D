2e76c642ee4db8fff25196129dc254c74acf356c
lefterisspetsakis <left.plan@gmail.com>
2017-02-18 16:32:33 -0500

    Added gamemanager initial commit

---
+++ JumpingBlock/Assets/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    GameManager GM;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        GM = GameManager.Instance;
+        GM.OnStateChange += HandleOnStateChange;
+	}
+
+    public void HandleOnStateChange()
+    {
+        Debug.Log("OnStateChange!");
+    }
+
+    public void OnGUI()
+    {
+        
+    }
+
+    public void ResumeGame()
+    {
+        GM.SetGameState(GameState.GAME);
+        Debug.Log(GM.gameState);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
